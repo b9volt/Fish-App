37126466e048da08fce65e0667aef486e8f0f438
jesseklein14 <jesseklein14@gmail.com>
2016-11-02 15:39:41 -0400

    login working

--- Fish_App_Api/config/routes.rb
+++ Fish_App_Api/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post '/api/user/login', to: 'user#login'
-  post '/api/user/logoff', to: 'user#logoff'
+  post '/users/login', to: 'users#login'
+  post '/users/logoff', to: 'users#logoff'
   # resources :trips
   resources :users, only: [:create] do
     resources :trips, only: [:create, :index, :show, :update, :destroy]
