fe005a277e279b53e6670d98fadea2b9ad6c74ec
Tom Murphree <tmurph1810@gmail.com>
2016-11-03 12:27:53 -0500

    Create load test data script
    Repass: get Faker to work

--- db/load_test_data.rb
+++ db/load_test_data.rb
@@ -1,21 +1,101 @@
-# Trip.destroy_all
+Trip.destroy_all
 
-array = []
-
-puts Faker::Random.number
-
-# 10.times do
-  Trip.create(
-    id: Faker::Random.number,
-    clientsHappy: Faker::Boolean,
-    end: Faker::Date.past,
-    numFishCaught: Faker::Random.number(30),
-    rating: Faker::Random.number(5),
-    start: Faker::Date.past,
-    summary: Faker::Hacker.say_something_smart,
+Trip.create(
+    clientsHappy: true,
+    end: "2015-12-15",
+    num_of_fish: 18,
+    rating: 2,
+    start: "2015-12-5",
+    summary: "You can't overr",
     user_id: 1,
-    weather: Faker::Lorem.sentence
-  )
-# end
+    weather: "reiciendis nam vel" )
 
-puts array
+  Trip.create(
+    clientsHappy: false,
+    end: "2016-05-22",
+    num_of_fish: 22,
+    rating: 3,
+    start: "2015-12-5",
+    summary: "Try to overr",
+    user_id: 1,
+    weather: "necessitatibus inc")
+
+  Trip.create(
+    clientsHappy: true,
+    end: "2016-02-22",
+    num_of_fish: 7,
+    rating: 3,
+    start: "2015-12-5",
+    summary: "Try to navigate the RSS matrix, maybe it will parse the online hard drive!",
+    user_id: 1,
+    weather: "necessitatibus inc")
+
+  Trip.create(
+    clientsHappy: false,
+    end: "2016-09-03",
+    num_of_fish: 23,
+    rating: 5,
+    start: "2015-12-5",
+    summary: "We need to navigate the virtual FTP bandw",
+    user_id: 1,
+    weather: "quas recusandae cumque" )
+
+  Trip.create(
+    clientsHappy: false,
+    end: "2016-03-11",
+    num_of_fish: 13,
+    rating: 5,
+    start: "2015-12-5",
+    summary: "ll program the primary USB microchip, that should driver the ADP circuit!",
+    user_id: 1,
+    weather: "ipsum voluptatem voluptatem" )
+
+  Trip.create(
+    clientsHappy: true,
+    end: "2016-01-15",
+    num_of_fish: 15,
+    rating: 2,
+    start: "2015-12-5",
+    summary: "The EXE feed is down, back up the 1080p matrix so we can generate the FTP system!",
+    user_id: 1,
+    weather: "eos error et" )
+
+  Trip.create(
+    clientsHappy: true,
+    end: "2016-09-15",
+    num_of_fish: 5,
+    rating: 3,
+    start: "2015-12-5",
+    summary: "The COM firewall is down, navigate the back-end hard drive so we can program the SMTP panel!",
+    user_id: 1,
+    weather: "doloremque illum quasi" )
+
+  Trip.create(
+    clientsHappy: true,
+    end: "2016-09-25",
+    num_of_fish: 19,
+    rating: 4,
+    start: "2015-12-5",
+    summary: "Try to back up the JBOD pixel, maybe it will program the haptic sensor!",
+    user_id: 1,
+    weather: "deserunt velit et" )
+
+  Trip.create(
+    clientsHappy: false,
+    end: "2016-08-12",
+    num_of_fish: 18,
+    rating: 0,
+    start: "2015-12-5",
+    summary: "programming the protocol won\"t do anything, we need to hack the open-source SAS microchip!",
+    user_id: 1,
+    weather: "facere sit voluptas" )
+
+  Trip.create(
+    clientsHappy: true,
+    end: "2015-12-19",
+    num_of_fish: 11,
+    rating: 2,
+    start: "2015-12-5",
+    summary: "You can't overr",
+    user_id: 1,
+    weather: "debitis quaerat doloribus" )
