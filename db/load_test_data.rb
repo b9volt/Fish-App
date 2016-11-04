e79746a288569b5c0ef47f01afdf1c0a36f6c6e7
Brad Volts <b9volt@aol.com>
2016-11-03 21:17:50 -0700

    Added seed data

---
+++ db/load_test_data.rb
@@ -0,0 +1,103 @@
+Trip.destroy_all
+
+Trip.create(
+  clientsHappy: true,
+  end: "2016-09-15",
+  num_of_fish: 5,
+  rating: 3,
+  start: "2016-09-15",
+  summary: "The COM firewall is down, navigate the back-end hard drive so we can program the SMTP panel!",
+  user_id: 1,
+  weather: "doloremque illum quasi" )
+
+Trip.create(
+  clientsHappy: true,
+  end: "2016-09-23",
+  num_of_fish: 15,
+  rating: 2,
+  start: "2016-09-23",
+  summary: "The EXE feed is down, back up the 1080p matrix so we can generate the FTP system!",
+  user_id: 1,
+  weather: "eos error et" )
+
+Trip.create(
+  clientsHappy: true,
+  end: "2016-09-25",
+  num_of_fish: 19,
+  rating: 4,
+  start: "2016-09-25",
+  summary: "Try to back up the JBOD pixel, maybe it will program the haptic sensor!",
+  user_id: 1,
+  weather: "deserunt velit et" )
+
+  Trip.create(
+    clientsHappy: false,
+    end: "2016-09-30",
+    num_of_fish: 18,
+    rating: 0,
+    start: "2016-09-30",
+    summary: "programming the protocol won\"t do anything, we need to hack the open-source SAS microchip!",
+    user_id: 1,
+    weather: "facere sit voluptas" )
+
+
+
+Trip.create(
+    clientsHappy: true,
+    end: "2016-10-01",
+    num_of_fish: 18,
+    rating: 2,
+    start: "2016-10-01",
+    summary: "You can't overr",
+    user_id: 1,
+    weather: "reiciendis nam vel" )
+
+  Trip.create(
+    clientsHappy: false,
+    end: "2016-10-02",
+    num_of_fish: 22,
+    rating: 3,
+    start: "2016-10-02",
+    summary: "Try to overr",
+    user_id: 1,
+    weather: "necessitatibus inc")
+
+  Trip.create(
+    clientsHappy: true,
+    end: "2016-10-03",
+    num_of_fish: 7,
+    rating: 3,
+    start: "2016-10-03",
+    summary: "Try to navigate the RSS matrix, maybe it will parse the online hard drive!",
+    user_id: 1,
+    weather: "necessitatibus inc")
+
+  Trip.create(
+    clientsHappy: false,
+    end: "2016-10-04",
+    num_of_fish: 23,
+    rating: 5,
+    start: "2016-10-04",
+    summary: "We need to navigate the virtual FTP bandw",
+    user_id: 1,
+    weather: "quas recusandae cumque" )
+
+  Trip.create(
+    clientsHappy: false,
+    end: "2016-10-05",
+    num_of_fish: 13,
+    rating: 5,
+    start: "2016-10-05",
+    summary: "ll program the primary USB microchip, that should driver the ADP circuit!",
+    user_id: 1,
+    weather: "ipsum voluptatem voluptatem" )
+
+  Trip.create(
+    clientsHappy: true,
+    end: "2016-10-15",
+    num_of_fish: 11,
+    rating: 2,
+    start: "2016-10-15",
+    summary: "You can't overr",
+    user_id: 1,
+    weather: "debitis quaerat doloribus" )
